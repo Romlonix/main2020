5d9d1e1fd4976a57e738e5cada766b6bf4cdff17
Romlonix <descarga7@hotmail.com>
2020-11-06 12:33:43 -0600

    v.1.8

--- js/jjss.js
+++ js/jjss.js
@@ -1,9 +1,3 @@
 // document.getElementById("").onclick = function () {
-
 //   document.getElementById("").classList.toggle("");
-
-//   document.getElementById("").classList.toggle("");
-
-//   document.getElementById("").classList.toggle("");
-
 // }
